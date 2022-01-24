74a486b439f948eb48afcde389db271f0cf3e8bc
diegoc23 <84074309+diegoc23@users.noreply.github.com>
2022-01-24 11:56:30 +0100

    Add files via upload

---
+++ serializerCangelosi/src/main/java/com/example/Main.java
@@ -0,0 +1,30 @@
+package com.example;
+import java.io.File;
+import java.io.IOException;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class Main 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        
+        //DESERIALIZZAZIONE
+
+        File file = new File("src/main/resources/classe.xml");
+        XmlMapper xmlMapper = new XmlMapper();
+        root value = xmlMapper.readValue(file, root.class);
+        System.out.println("La classe " + value.getClasse() + " " + value.getSpecializzazione() + " si trova nell'aula " + value.getAula().getNome() + " ed è composta dai seguenti studenti: ");
+
+        for (int i = 0; i < value.getStudenti().size(); i++) {
+            System.out.println("- " + value.getStudenti().get(i).getCognome());
+
+        }
+
+
+        //SERIALIZZAZIONE
+
+        xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        xmlMapper.writeValue(new File("src/main/resources/classe_serializzata.xml"), value);
+    }
+}
---
+++ serializerCangelosi/src/main/java/com/example/aula.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class aula 
+{
+ 
+        String nome;
+        String numeroDiBanchi;
+    
+        public String getNome()
+        {
+            return nome;
+        }
+    
+        public String getNumeroDiBanchi() 
+        {
+            return numeroDiBanchi;
+        }
+    }
+
